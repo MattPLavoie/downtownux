5c66f7ea4c6f31f4a85857e8543d2a553e3b4cc2
Matt Lavoie <mattplavoie@gmail.com>
2016-05-09 23:24:53 -0400

    Everything but fixed nav

--- js/conf.js
+++ js/conf.js
@@ -1,5 +1,17 @@
 app.controller('confController', ['$scope', '$http', confController]);
 
-function confController($scope, $http, $q) {
-  
+function confController($scope, $http) {
+  Tabletop.init( {
+        key: '13vt27jO41_sjDlpjatEvWkEtx-Ri4GvaqUGfIEJ7Gck',
+        callback: function(data, tabletop) {
+          $scope.schedule = data;
+          $scope.$digest();
+        },
+        simpleSheet: true,
+        orderby: 'time'
+      } );
+
+  $scope.scrollTo = function(el) {
+    $('body').animate({ scrollTop: $('.' + el).offset().top }, 600);
+  };
 }
